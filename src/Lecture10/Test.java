0287ea160fbdda7cf0c3b5c3d4451a012b8130b3
aksamiftikhar <aksam.iftikhar@gmail.com>
2025-10-01 13:10:37 +0500

    Lecture 10 completed

---
+++ src/Lecture10/Test.java
@@ -0,0 +1,58 @@
+package Lecture10;
+import static java.lang.Math.*;
+//import static java.lang.Math.sqrt;
+
+public class Test {
+    int x;
+
+    final int f;
+
+    {
+        f = 100;
+        System.out.println("f init in non-static init block = " + f);
+    }
+    public Test()
+    {
+        System.out.println("Test constructor");
+        System.out.println(f);
+    }
+    public Test(int x)
+    {
+
+    }
+    private static int myStatic;
+    public int getMyStatic() {
+        return myStatic;
+    }
+    public String toString() {
+        return "X is : " + x;
+    }
+
+    public static void main(String[] args) {
+        Test t1 = new Test();
+//        t1.x = 30;
+
+        Test t2 = new Test();
+
+//        System.out.println(Test.myStatic);
+//        System.out.println(t1);
+//
+//        String s1 = t1.toString();
+//
+//        double f = sqrt(3);
+//        double d = round(3.15);
+
+    }
+
+}
+
+class Test2
+{
+    public Test2()
+    {
+        Test t1 = new Test();
+
+
+    }
+}
+
---
+++ src/Lecture9/Test.java
@@ -0,0 +1,10 @@
+package Lecture9;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        // Lecture was conducted on board due to unavailability of multimedia, so no demo
+    }
+}
+
